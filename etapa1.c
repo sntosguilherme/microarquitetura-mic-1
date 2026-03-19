0b718810ae1eb86781e09d95e1707e9d1f5c086b
João Guilherme <sntos.guilherme9705@gmail.com>
2026-03-19 01:03:35 -0300

    feat comentario nos componentes e prototipo de função

---
+++ etapa1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+/* OQ FAZ CADA COMPONENTE
+A: OPERANDO 1
+B: OPERANDO 2
+ENA: Enable A. Quando está em 0, qualquer valor em A é ignorado e se torna 0. 
+ENB: Enable B. Quando está em 0, qualquer valor em B é ignorado e se torna 0.
+INVA: Inverte A bit a bit.
+INC: Guarda o vai-um. Soma 1 ao resultado final.
+F0 e F1: Em conjunto definem a operação a ser realizada.
+ */
+
+
+int main(){
+    // leitura do arquivo
+
+
+    return 0;
+}
