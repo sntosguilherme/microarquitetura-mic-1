b36a3381b10df38085b865f8e89bf92d28c2b985
LaraGGomes <laragomes471@gmail.com>
2026-03-28 18:34:04 -0300

    feat: etapa3

--- Etapa3/etapa3Tar1.c
+++ Etapa3/etapa3Tar1.c
@@ -1,4 +1,4 @@
-#include "../Etapa2/etapa2Tar2.h" 
+#include "etapa3.h" 
 
 /* 
 NOVO FORMATO DA MICROINSTRUÇÃO - 23 bits
@@ -19,22 +19,32 @@ Os dois não podem estar em nível lógico alto simultaneamente.
 */
 
 int main() {
-    char h[]   = "00000000000000000000000000000001";
-    char opc[] = "00000000000000000000000000000000";
-    char tos[] = "00000000000000000000000000000010";
-    char cpp[] = "00000000000000000000000000000000";
-    char lv[]  = "00000000000000000000000000000000";
-    char sp[]  = "00000000000000000000000000000000";
-    char pc[]  = "00000000000000000000000000000000";
-    char mdr[] = "00000000000000000000000000000000";
-    char mar[] = "00000000000000000000000000000000";
-    char mbr[] = "10000001";
+    int PC = 0;     // contador de programa
     char IR[25];    // Agora para 23 bits + \n + \0
-    char s[33];
-    char sd[33];
-    char co = '0';
+    char s[33];     // saida
+    char sd[33];    // saida deslocada
+    int f0;
+    int f1;
+    int sll8;       // deslocamento de 8 bits para esquerda
+    int sra1;       // deslocamento aritmético em 1 bit pra direita
+    char co;
     char memoria_dados[8][33]; // Memória de 8 linhas de 32 bits.
 
+    char a[33]; 
+    char b[33];
+
+    int ciclo = 1;
+
+    char h[]   = "00000000000000000000000000000000";
+    char opc[] = "00000000000000000000000000000000";
+    char tos[] = "00000000000000000000000000000000";
+    char cpp[] = "00000000000000000000000000000000";
+    char lv[]  = "00000000000000000000000000000000";
+    char sp[]  = "00000000000000000000000000000100";
+    char pc[]  = "00000000000000000000000000000000";
+    char mdr[] = "00000000000000000000000000000000";
+    char mar[] = "00000000000000000000000000000100";
+    char mbr[] = "00000000";
 
     // Carregando inicialmente a memória. Cada loop pega uma linha inteira do arquivo.
     FILE *dados = fopen("entrada-e-saida-etapa3/dados_etapa3_tarefa1.txt", "r");
@@ -67,6 +77,51 @@ int main() {
     }
 
     // Converter os 4 ultimos bits de MAR para um número de 0 a 15 correspondente a linha da memória. deve ser posto no loop de execução
+
+    while (fgets(IR, sizeof(IR), leitura) != NULL) {
+        char b_bus[5];
+        char c_bus[10];
+        
+        co = '0';
+        sll8 = IR[0] - '0';
+        sra1 = IR[1] - '0';
+        f0 = IR[2] - '0';
+        f1 = IR[3] - '0';
+
+        if (erro2(ciclo, IR, log)) 
+            continue;
+        
+        strcpy(a, h);
+        
+        barramentoBE3(IR, b, b_bus, opc, tos, cpp, lv, sp, pc, mdr, mar, mbr);
+
+        processarC_bus(IR, c_bus);
+
+        logEntradaE3(memoria_dados, IR, ciclo, b_bus, c_bus, mar, mdr, pc, mbr, sp, lv, cpp, tos, opc, h, log);
+        
+        processamentoEntradas2(IR, a, b);
+
+        calculoULA(f0, f1, a, b, &co, s);
+
+        if (IR[7] == '1') incremento(s, &co);
+
+        strcpy(sd, s);
+
+        if (sll8) deslocaSLL8(sd);
+        if (sra1) deslocaSRA1(sd);
+
+        barramentoC(IR, sd, c_bus, h, opc, tos, cpp, lv, sp, pc, mdr, mar);
+
+        wrMemoria(IR, mar, mdr, memoria_dados);
+
+        logFinalE3(memoria_dados, mar, mdr, pc, mbr, sp, lv, cpp, tos, opc, h, log);
+
+        ciclo++;
+    }
+
+    printf("log armazenado com sucesso em entrada-e-saida/saida.txt.\n");
+    fclose(leitura);
+    fclose(log);
 
     return 0;
 }
